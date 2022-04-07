ef085900b3901f73e5f4042bf9f6ea1aeb2d004f
Haris Anwar <>
2022-04-07 16:05:53 +0500

    test: grid available unit test

---
+++ tests/grid_test.py
@@ -0,0 +1,15 @@
+import unittest
+from simulation import RoboVsDino
+
+
+class TestUser(unittest.TestCase):
+
+    def test_grid_spot_availability(self):
+        RoboVsDino().create_grid()
+        RoboVsDino().create_single_robo((2, 4), "Up")
+        available = RoboVsDino().check_grid_spot_available((2, 4))
+        self.assertEqual(len(available), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
