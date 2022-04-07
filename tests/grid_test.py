83b0f32c91fca15dcf41be472ca00055c8ee88e1
Haris Anwar <>
2022-04-07 23:45:33 +0500

    test: unit test for create entity and grid availability

--- tests/grid_test.py
+++ tests/grid_test.py
@@ -5,11 +5,13 @@ from app.simulation import RoboVsDino
 class TestUser(unittest.TestCase):
 
     def test_grid_spot_availability(self):
-        RoboVsDino().create_grid()
-        RoboVsDino().create_single_robo((2, 4), "Up")
-        available = RoboVsDino().check_grid_spot_available((2, 4))
-        self.assertEqual(len(available), 0)
+        # RoboVsDino().create_grid()
+        # RoboVsDino().create_single_robo((2, 4), "Up")
+        available = RoboVsDino().check_grid_spot_available((2, 4), 'app/simulation_data/robo_vs_dino.json')
+        self.assertNotEqual(len(available), 0)
 
 
 if __name__ == '__main__':
+    # todo: more test cases can be written for cases like 'is dinosaur actually killed after attack', 'direction updates
+    #  successfully or not', 'only robot kill dinosaur' etc.
     unittest.main()
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,5 +1,6 @@
 import unittest
 from run import app
+import json
 
 
 class FlaskTest(unittest.TestCase):
@@ -13,6 +14,25 @@ class FlaskTest(unittest.TestCase):
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
 
+    def test_create_entity(self):
+        tester = app.test_client(self)
+        test_payload = {
+            "description": "unit test desc",
+            "direction": "Left",
+            "id": "xyz",
+            "type": "R",
+            "x_cord": 45,
+            "y_cord": 21
+        }
+        response = tester.post("webapp/create_entity", data=json.dumps(test_payload),
+                               headers={'Content-Type': 'application/json'})
+        statuscode = response.status_code
+        self.assertEqual(
+            200,
+            statuscode
+        )
+
 
 if __name__ == '__main__':
+    # todo: more test cases can be written for 'issue instructiion to robot' etc.
     unittest.main()
